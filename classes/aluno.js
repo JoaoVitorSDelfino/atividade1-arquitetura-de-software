8724312eed9c9eae2ae71193dd4606af570635c7
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2024-03-16 14:52:54 -0300

    feat: inicialização das classes aluno e turma

---
+++ classes/aluno.js
@@ -0,0 +1,11 @@
+class Aluno {
+    nome = 'default'
+    login = '123'
+    ra = 321
+
+    constructor(nome, login, ra) {
+        this.nome = nome
+        this.login = login
+        this.ra = ra
+    }
+}
---
+++ classes/turma.js
@@ -0,0 +1,9 @@
+class Turma {
+    #codigo = 123
+    #nota = 0
+
+    constructor(codigo, nota) {
+        this.#codigo = codigo
+        this.#nota = nota
+    }
+}
